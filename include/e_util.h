1df00392c715a55075a2594223ca95f42791fa18
Rosie Sapphire <jayjaygames40@gmail.com>
2023-06-02 18:51:24 -0500

    Added more mat4 functions

--- include/e_util.h
+++ include/e_util.h
@@ -18,9 +18,12 @@ void vec2_copy(vec2 src, vec2 dst);
 void vec2_sub(vec2 a, vec2 b, vec2 o);
 void vec3_copy(vec3 src, vec3 dst);
 void vec3_add(vec3 a, vec3 b, vec3 o);
+void mat4_copy(mat4 src, mat4 dst);
 void mat4_zero(mat4 m);
 void mat4_identity(mat4 m);
 void mat4_trans(mat4 m, vec3 v);
+void mat4_mul(mat4 a, mat4 b, mat4 o);
+void mat4_rot(mat4 m, vec3 axis, float angle_rad);
 void mat4_perspective(float fovd, float aspect,
 		float near, float far, mat4 out);
 
--- src/e_util.c
+++ src/e_util.c
@@ -43,6 +43,11 @@ void vec3_add(vec3 a, vec3 b, vec3 o)
 	o[2] = a[2] + b[2];
 }
 
+void mat4_copy(mat4 src, mat4 dst)
+{
+	bcopy(src, dst, sizeof(mat4));
+}
+
 void mat4_zero(mat4 m)
 {
 	bzero(m, sizeof(mat4));
@@ -58,6 +63,43 @@ void mat4_identity(mat4 m)
 void mat4_trans(mat4 m, vec3 v)
 {
 	vec3_add(m[3], v, m[3]);
+}
+
+void mat4_mul(mat4 a, mat4 b, mat4 o)
+{
+	mat4 a_tmp;
+	mat4_copy(a, a_tmp);
+	mat4_zero(o);
+
+	for(int row = 0; row < 4; row++)
+		for(int col = 0; col < 4; col++)
+			for(int ind = 0; ind < 4; ind++)
+				o[col][row] += a_tmp[ind][row] * b[col][ind];
+}
+
+void mat4_rot(mat4 m, vec3 axis, float angle_rad)
+{
+	mat4 rot;
+	mat4_zero(rot);
+
+	const float angle_sin = sinf(angle_rad);
+	const float angle_cos = cosf(angle_rad);
+
+	rot[0][0] = angle_cos + (axis[0] * axis[0]) * (1 - angle_cos);
+	rot[1][0] = axis[0] * axis[1] * (1 - angle_cos) - axis[2] * angle_sin;
+	rot[2][0] = axis[0] * axis[2] * (1 - angle_cos) + axis[1] * angle_sin;
+
+	rot[0][1] = axis[1] * axis[0] * (1 - angle_cos) + axis[2] * angle_sin;
+	rot[1][1] = angle_cos + (axis[1] * axis[1]) * (1 - angle_cos);
+	rot[2][1] = axis[1] * axis[2] * (1 - angle_cos) - axis[0] * angle_sin;
+
+	rot[0][2] = axis[2] * axis[0] * (1 - angle_cos) - axis[1] * angle_sin;
+	rot[1][2] = axis[2] * axis[1] * (1 - angle_cos) + axis[0] * angle_sin;
+	rot[2][2] = angle_cos + (axis[2] * axis[2]) * (1 - angle_cos);
+
+	rot[3][3] = 1.0f;
+
+	mat4_mul(m, rot, m);
 }
 
 void mat4_perspective(float fovd, float aspect,
