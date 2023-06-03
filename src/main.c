a1c28585ed9ed455a5412c3b706df2fe49d51a27
Rosie Sapphire <jayjaygames40@gmail.com>
2023-06-03 18:29:25 -0500

    Moved all the funny shader shit into the model draw function

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WIDTH 1280
-#define HEIGHT 720
+#define WIDTH 1920
+#define HEIGHT 1080
 #define ASPECT_RATIO ((float)WIDTH / (float)HEIGHT)
 
 int main(void)
@@ -20,17 +20,11 @@ int main(void)
 
 	uint base_shader =
 		shader_create("shaders/base.vert", "shaders/base.frag");
-	int proj_loc =      shader_get_loc(base_shader, "u_proj");
-	int view_loc =      shader_get_loc(base_shader, "u_view");
-	int model_loc =     shader_get_loc(base_shader, "u_model");
-	int tex_loc =       shader_get_loc(base_shader, "u_tex");
-	int using_tex_loc = shader_get_loc(base_shader, "u_is_using_tex");
 
 	mat4 proj;
 	mat4_perspective(90.0f, ASPECT_RATIO, 0.1f, 128, proj);
 	struct camera cam = {0};
 	struct model room = model_load("models/room.glb");
-	uint room_tex = texture_load("textures/checker.png");
 	struct input input = {0};
 
 	float time_last = context_get_time();
@@ -42,19 +36,9 @@ int main(void)
 		cam = camera_update_axis(cam, input);
 		cam = camera_move(cam, input, delta_time);
 
-		shader_use(base_shader);
-
-		mat4 model;
-		model_get_mat4(room, model);
-		shader_uni_mat4(model_loc, model);
-
 		mat4 view;
 		cam = camera_get_mat4(cam, view, delta_time);
-		shader_uni_mat4(view_loc, view);
-		shader_uni_mat4(proj_loc, proj);
-		shader_uni_int(tex_loc, 0);
-		shader_uni_int(using_tex_loc, true);
-		model_draw(room, room_tex);
+		model_draw(room, 0, base_shader, proj, view);
 		rlayer_unbind_all();
 		rlayer_draw(layer);
 
@@ -66,7 +50,6 @@ int main(void)
 		time_last = time_now;
 	}
 
-	texture_unload(&room_tex);
 	rlayer_destroy(&layer);
 	rlayers_terminate();
 	shader_destroy(base_shader);
