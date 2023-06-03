e9fa9de392e7b05e5dbd285310e2fc7d7b1c16e5
Rosie Sapphire <jayjaygames40@gmail.com>
2023-06-03 13:58:18 -0500

    Shuffled some things around and changed some values inmain

--- src/main.c
+++ src/main.c
@@ -3,12 +3,13 @@
 #include "e_shader.h"
 #include "e_model.h"
 #include "e_camera.h"
+#include "e_texture.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WIDTH 1920
-#define HEIGHT 1080
+#define WIDTH 1280
+#define HEIGHT 720
 #define ASPECT_RATIO ((float)WIDTH / (float)HEIGHT)
 
 int main(void)
@@ -22,11 +23,14 @@ int main(void)
 	int proj_loc =      shader_get_loc(base_shader, "u_proj");
 	int view_loc =      shader_get_loc(base_shader, "u_view");
 	int model_loc =     shader_get_loc(base_shader, "u_model");
+	int tex_loc =       shader_get_loc(base_shader, "u_tex");
+	int using_tex_loc = shader_get_loc(base_shader, "u_is_using_tex");
 
 	mat4 proj;
-	mat4_perspective(70.0f, ASPECT_RATIO, 0.1f, 10.0f, proj);
+	mat4_perspective(90.0f, ASPECT_RATIO, 0.1f, 128, proj);
 	struct camera cam = {{0, 0, 0}, {0, 0, 0}, 0, 0};
 	struct model room = model_load("models/room.glb");
+	uint room_tex = texture_load("textures/checker.png");
 	struct input input = {0};
 
 	float time_last = context_get_time();
@@ -35,22 +39,22 @@ int main(void)
 	while(context_is_running()) {
 		rlayer_bind_and_clear(layer, 0.05f, 0.1f, 0.2f, 1.0f);
 
-		mat4 view;
-		mat4 model;
-		mat4_identity(view);
-		mat4_trans(view, (vec3){0, 0, -6});
-
-		model_get_mat4(room, model);
-
 		cam = camera_update_axis(cam, input);
 		cam = camera_move(cam, input, delta_time);
-		camera_get_mat4(cam, view);
 
 		shader_use(base_shader);
+
+		mat4 model;
+		model_get_mat4(room, model);
 		shader_uni_mat4(model_loc, model);
+
+		mat4 view;
+		camera_get_mat4(cam, view);
 		shader_uni_mat4(view_loc, view);
 		shader_uni_mat4(proj_loc, proj);
-		model_draw(room, 0);
+		shader_uni_int(tex_loc, 0);
+		shader_uni_int(using_tex_loc, true);
+		model_draw(room, room_tex);
 		rlayer_unbind_all();
 		rlayer_draw(layer);
 
@@ -62,6 +66,7 @@ int main(void)
 		time_last = time_now;
 	}
 
+	texture_unload(&room_tex);
 	rlayer_destroy(&layer);
 	rlayers_terminate();
 	shader_destroy(base_shader);
